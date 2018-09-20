0d4e91c67090b5785e578950a494b5ec7f0692be
Akiva Rosenzweig <akiva@chatnbike.com>
2018-09-19 22:36:47 -0400

    Added image to background
    Also made text white, formatted the image, etc.

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -6,13 +6,15 @@ export default class Home extends Component {
 
   render () {
     return (
-      <div id="wrapper">
+
         <div className="titles">
-          <h1> See the World In a New Light. </h1>
-          <h2> Welcome to my gallery. </h2>
-          <h3> Akiva Rosenzweig </h3>
+          <div>
+            <h1> See the World In a New Light. </h1>
+            <h2> Welcome to my gallery. </h2>
+            <h3> Akiva Rosenzweig </h3>
+          </div>
         </div>
-      </div>
+
 
     )
   }
