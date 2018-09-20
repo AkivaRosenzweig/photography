012ae93a90508f9cee711749c062f7ec753b5df8
Akiva Rosenzweig <akiva@chatnbike.com>
2018-09-19 21:33:02 -0400

    Added font and formatting to title

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -6,10 +6,12 @@ export default class Home extends Component {
 
   render () {
     return (
-      <div>
-        <h1> See the World In a New Light. </h1>
-        <h2> Welcome to my gallery. </h2>
-        <h3> Akiva Rosenzweig </h3>
+      <div id="wrapper">
+        <div className="titles">
+          <h1> See the World In a New Light. </h1>
+          <h2> Welcome to my gallery. </h2>
+          <h3> Akiva Rosenzweig </h3>
+        </div>
       </div>
 
     )
