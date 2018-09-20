edf2444278d4011c7164c894efb2c4ac79c8376e
Akiva Rosenzweig <akiva@chatnbike.com>
2018-09-20 00:14:21 -0400

    Changed title slogan?

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
 
         <div className="titles">
           <div>
-            <h1> See the World In a New Light. </h1>
+            <h1> See the world through a {"new"} lense.</h1>
             <h2> Welcome to my gallery. </h2>
             <h3> Akiva Rosenzweig </h3>
           </div>
