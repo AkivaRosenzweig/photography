9912a702b91ca4ed56112c261e0f59a6288e99fe
Akiva Rosenzweig <akiva@chatnbike.com>
2018-09-20 23:42:24 -0400

    Styled title again. added down button.

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -8,11 +8,12 @@ export default class Home extends Component {
     return (
 
         <div className="titles">
-          <div>
-            <h1> See the world through a {"new"} lense.</h1>
-            <h2> Welcome to my gallery. </h2>
-            <h3> Akiva Rosenzweig </h3>
-          </div>
+          <header>
+            <h1 className="big-font small-margin"> See the world through a {"new"} lense.</h1>
+            <h2 className="medium-font small-margin"> Welcome to my gallery. </h2>
+            <h3 className="small-font small-margin"> Akiva Rosenzweig </h3>
+          </header>
+          <button className="transparent-button">&darr;</button>
         </div>
 
 
