75b36273450b9b680e58a6e6d281c823301ce0f0
Akiva Rosenzweig <akiva@chatnbike.com>
2018-09-21 23:20:51 -0400

    Reformatted home page and added button fade.
    made titles table, made header table cell, so that we could have stuff after the top introduction.

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -6,16 +6,26 @@ export default class Home extends Component {
 
   render () {
     return (
-
+      <React.Fragment>
         <div className="titles">
           <header>
-            <h1 className="big-font small-margin"> See the world through a {"new"} lense.</h1>
-            <h2 className="medium-font small-margin"> Welcome to my gallery. </h2>
-            <h3 className="small-font small-margin"> Akiva Rosenzweig </h3>
+            <h1 className="big-font no-margin"> See the world through a new lense.</h1>
+            <h2 className="medium-font no-margin"> Welcome to my gallery. </h2>
+            <h3 className="small-font no-margin"> Akiva Rosenzweig </h3>
+            <button className="transparent-button">&darr;</button>
           </header>
-          <button className="transparent-button">&darr;</button>
+
         </div>
 
+
+
+        <div className="bottom">
+          <h3 className="no-margin">Travel</h3>
+          <p> I love taking photos when I travel because places far
+          away from home are always interesting and new.</p>
+        </div>
+
+      </React.Fragment>
 
     )
   }
